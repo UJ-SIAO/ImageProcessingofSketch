7d586602b1cd8ed7e58a2419a7eb67852b7f19ee
UJ-SIAO <71709501+UJ-SIAO@users.noreply.github.com>
2021-05-27 20:52:17 +0800

    重新上傳_優化v1.0
    重新上傳_優化v1.0

--- ImageProcessing.runs/impl_1/imageProcessTop.tcl
+++ ImageProcessing.runs/impl_1/imageProcessTop.tcl
@@ -42,26 +42,93 @@ proc step_failed { step } {
   close $ch
 }
 
-set_msg_config -id {Common 17-41} -limit 10000000
 set_msg_config -id {HDL 9-1061} -limit 100000
 set_msg_config -id {HDL 9-1654} -limit 100000
-set_msg_config -id {HDL-1065} -limit 10000
 
-start_step write_bitstream
+start_step init_design
 set rc [catch {
-  create_msg_db write_bitstream.pb
-  open_checkpoint imageProcessTop_routed.dcp
+  create_msg_db init_design.pb
+  create_project -in_memory -part xc7z020clg484-1
+  set_property board_part em.avnet.com:zed:part0:1.3 [current_project]
+  set_property design_mode GateLvl [current_fileset]
+  set_param project.singleFileAddWarning.threshold 0
   set_property webtalk.parent_dir D:/Verilog/ImageProcessingofSketch/ImageProcessing.cache/wt [current_project]
-  catch { write_mem_info -force imageProcessTop.mmi }
-  write_bitstream -force imageProcessTop.bit 
-  catch { write_sysdef -hwdef imageProcessTop.hwdef -bitfile imageProcessTop.bit -meminfo imageProcessTop.mmi -file imageProcessTop.sysdef }
-  catch {write_debug_probes -quiet -force debug_nets}
-  close_msg_db -file write_bitstream.pb
+  set_property parent.project_path D:/Verilog/ImageProcessingofSketch/ImageProcessing.xpr [current_project]
+  set_property ip_repo_paths {
+  d:/Verilog/ImageProcessingofSketch/ImageProcessing.cache/ip
+  D:/Verilog/ImageProcessingofSketch
+  D:/Verilog/ImageProcessingofSketch/ImageProcessing.srcs/sources_1
+  D:/Verilog/ImageProcessingofSketch/ImageProcessing.sim/sim_1/behav/Xilinx/vivado-library-master
+} [current_project]
+  set_property ip_output_repo d:/Verilog/ImageProcessingofSketch/ImageProcessing.cache/ip [current_project]
+  set_property XPM_LIBRARIES XPM_MEMORY [current_project]
+  add_files -quiet D:/Verilog/ImageProcessingofSketch/ImageProcessing.runs/synth_1/imageProcessTop.dcp
+  add_files -quiet d:/Verilog/ImageProcessingofSketch/ImageProcessing.srcs/sources_1/ip/outputBuffer/outputBuffer.dcp
+  set_property netlist_only true [get_files d:/Verilog/ImageProcessingofSketch/ImageProcessing.srcs/sources_1/ip/outputBuffer/outputBuffer.dcp]
+  read_xdc -mode out_of_context -ref outputBuffer -cells U0 d:/Verilog/ImageProcessingofSketch/ImageProcessing.srcs/sources_1/ip/outputBuffer/outputBuffer_ooc.xdc
+  set_property processing_order EARLY [get_files d:/Verilog/ImageProcessingofSketch/ImageProcessing.srcs/sources_1/ip/outputBuffer/outputBuffer_ooc.xdc]
+  read_xdc -ref outputBuffer -cells U0 d:/Verilog/ImageProcessingofSketch/ImageProcessing.srcs/sources_1/ip/outputBuffer/outputBuffer/outputBuffer.xdc
+  set_property processing_order EARLY [get_files d:/Verilog/ImageProcessingofSketch/ImageProcessing.srcs/sources_1/ip/outputBuffer/outputBuffer/outputBuffer.xdc]
+  link_design -top imageProcessTop -part xc7z020clg484-1
+  write_hwdef -file imageProcessTop.hwdef
+  close_msg_db -file init_design.pb
 } RESULT]
 if {$rc} {
-  step_failed write_bitstream
+  step_failed init_design
   return -code error $RESULT
 } else {
-  end_step write_bitstream
+  end_step init_design
+}
+
+start_step opt_design
+set rc [catch {
+  create_msg_db opt_design.pb
+  opt_design 
+  write_checkpoint -force imageProcessTop_opt.dcp
+  report_drc -file imageProcessTop_drc_opted.rpt
+  close_msg_db -file opt_design.pb
+} RESULT]
+if {$rc} {
+  step_failed opt_design
+  return -code error $RESULT
+} else {
+  end_step opt_design
+}
+
+start_step place_design
+set rc [catch {
+  create_msg_db place_design.pb
+  implement_debug_core 
+  place_design 
+  write_checkpoint -force imageProcessTop_placed.dcp
+  report_io -file imageProcessTop_io_placed.rpt
+  report_utilization -file imageProcessTop_utilization_placed.rpt -pb imageProcessTop_utilization_placed.pb
+  report_control_sets -verbose -file imageProcessTop_control_sets_placed.rpt
+  close_msg_db -file place_design.pb
+} RESULT]
+if {$rc} {
+  step_failed place_design
+  return -code error $RESULT
+} else {
+  end_step place_design
+}
+
+start_step route_design
+set rc [catch {
+  create_msg_db route_design.pb
+  route_design 
+  write_checkpoint -force imageProcessTop_routed.dcp
+  report_drc -file imageProcessTop_drc_routed.rpt -pb imageProcessTop_drc_routed.pb
+  report_timing_summary -warn_on_violation -max_paths 10 -file imageProcessTop_timing_summary_routed.rpt -rpx imageProcessTop_timing_summary_routed.rpx
+  report_power -file imageProcessTop_power_routed.rpt -pb imageProcessTop_power_summary_routed.pb -rpx imageProcessTop_power_routed.rpx
+  report_route_status -file imageProcessTop_route_status.rpt -pb imageProcessTop_route_status.pb
+  report_clock_utilization -file imageProcessTop_clock_utilization_routed.rpt
+  close_msg_db -file route_design.pb
+} RESULT]
+if {$rc} {
+  step_failed route_design
+  return -code error $RESULT
+} else {
+  end_step route_design
 }
 
